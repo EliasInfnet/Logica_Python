7881ed2893909f1d0cb17c0228cb41f04f467765
EliasInfnet <elias@uakti.com>
2021-09-22 18:49:51 -0300

    segundo commit

--- q9.py
+++ q9.py
@@ -6,7 +6,7 @@ html = requests.get("https://fgopassos.github.io/pagina_exemplo/estadosCentroOes
 
 soup = BeautifulSoup(html, 'html.parser')
 
-sigla = str(input("Entre com uma sigla de estado do centro oeste: "))
+
 
 lista_estados = []
 
@@ -20,12 +20,27 @@ for i in linha:
         estado.append(x.string)
     lista_estados.append(estado)    
 
-print(sigla)
+print("Lista de estados")
+
+print('------------------')
+
+for row in lista_estados:
+    print(row)
+
+print('==================')
+
+sigla = str(input("Entre com uma sigla de estado do centro oeste: "))
+
+print('Dados do estado que você escolheu :')
+print('')
+
 if(lista_estados[0][0] == sigla):
-    print(lista_estados[0])
-    if(lista_estados[1][1] == sigla):
-        print(lista_estados[1])
-        if(lista_estados[2][2] == sigla):
-            print(lista_estados[2])   
-            if(lista_estados[3][3] == sigla):
-                print(lista_estados[3])
+  print(lista_estados[0])
+if(lista_estados[1][0] == sigla):
+  print(lista_estados[1])
+if(lista_estados[2][0] == sigla):
+  print(lista_estados[2])   
+if(lista_estados[3][0] == sigla):
+  print(lista_estados[3])
+
+print('------------------')
